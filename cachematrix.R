2ed2b58bf2016a2856bc6534f6d9924989726553
anamika-alt <65449465+anamika-alt@users.noreply.github.com>
2020-05-16 17:43:18 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,15 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+inver <- NULL
+  set <- function(y){
+    x <<- y
+    inver <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inver <<- solveMatrix
+  getInverse <- function() inver
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+inver <- x$getInverse()
+  if(!is.null(inver)){
+    message("getting cached data")
+    return(inver)
+  }
+  data <- x$get()
+  inver <- solve(data)
+  x$setInverse(inver)
+  inver      
+}
 }
